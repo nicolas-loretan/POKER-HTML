ed32b646c7233ce4f12b0079c491ba609f1caf76
nicolas-loretan <nicolas.loretan@edu.vs.ch>
2025-05-10 12:55:39 +0200

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,6 +1,9 @@
+function nouvellePartie(){
+}
+
+
 // Bouton
 const bouton = document.createElement("button");
 bouton.textContent = "Nouvelle partie";
 bouton.addEventListener("click", nouvellePartie);
 document.body.appendChild(bouton);
-
