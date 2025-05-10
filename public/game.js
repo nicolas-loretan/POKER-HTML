e1638403035f222ccced62acf21dd8d2957505b6
nicolas-loretan <nicolas.loretan@edu.vs.ch>
2025-05-10 22:45:05 +0200

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000"); // io ou io("http://localhost:3000") si pas même origine
+const socket = io; // io ou io("http://localhost:3000") si pas même origine
 const playForm = document.getElementById('playForm');
 const _ensembleJoueurs = document.getElementById("ensembleJoueurs");
 const _river = document.getElementById("river");
